504ac65504ed7532524fcde185eb579b7412a18a
woodam <rsc3399@nate.com>
2016-10-29 21:28:18 +0900

    boot default

--- src/main/java/com/dam/controller/TestController.java
+++ src/main/java/com/dam/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by woodam on 2016. 10. 23..
@@ -14,7 +15,7 @@ public class TestController {
     @Autowired
     TestService testService;
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", method= RequestMethod.GET)
     public String test(Model model){
         model.addAttribute("foo", testService.getText() );
         return "test";
